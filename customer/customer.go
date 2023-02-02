6ce4d270f7bc81dbeb74c555edbb6e33a3330247
Yibo Fu <yf091c@att.com>
2023-02-01 23:44:07 -0800

    complete operation and customer functions

--- customer/customer.go
+++ customer/customer.go
@@ -1,1 +1,34 @@
 package customer
+
+type Customer struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Role      string `json:"role"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Contacted bool   `json:"contacted"`
+}
+
+// CreateCustomer creates a new customer
+func CreateCustomer(ID, Name, Role, Email, Phone string, Contacted bool) (result *Customer) {
+	customer := Customer{
+		ID:        ID,
+		Name:      Name,
+		Role:      Role,
+		Email:     Email,
+		Phone:     Phone,
+		Contacted: Contacted,
+	}
+	result = &customer
+	return result
+}
+
+// ModifyCustomer modifies an existing customer
+func (c *Customer) ModifyCustomer(ID, Name, Role, Email, Phone string, Contacted bool) {
+	c.ID = ID
+	c.Name = Name
+	c.Role = Role
+	c.Email = Email
+	c.Phone = Phone
+	c.Contacted = Contacted
+}
