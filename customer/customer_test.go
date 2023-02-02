c3d0776bdda941bf5af159fe42e5ca9b3845ac49
Yibo Fu <yf091c@att.com>
2023-02-02 00:36:51 -0800

    add unit test for customer

--- customer/customer_test.go
+++ customer/customer_test.go
@@ -1,1 +1,47 @@
 package customer
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Unit Test for Customer")
+}
+
+var _ = Describe("Customer Test", func() {
+
+	BeforeEach(func() {
+
+	})
+	AfterEach(func() {
+
+	})
+
+	Context("Customer function test", func() {
+		It("Test CreateCustomer", func() {
+			ca := CreateCustomer("1", "Andy", "Developer", "S", "S", true)
+
+			Expect(ca.ID).To(Equal("1"))
+			Expect(ca.Name).To(Equal("Andy"))
+			Expect(ca.Role).To(Equal("Developer"))
+			Expect(ca.Email).To(Equal("S"))
+			Expect(ca.Phone).To(Equal("S"))
+			Expect(ca.Contacted).To(Equal(true))
+		})
+
+		It("Test ModifyCustomer", func() {
+			ca := CreateCustomer("1", "Andy", "Developer", "S", "S", true)
+			ca.ModifyCustomer("2", "Peter", "FBI", "S", "S", true)
+
+			Expect(ca.ID).To(Equal("2"))
+			Expect(ca.Name).To(Equal("Peter"))
+			Expect(ca.Role).To(Equal("FBI"))
+			Expect(ca.Email).To(Equal("S"))
+			Expect(ca.Phone).To(Equal("S"))
+			Expect(ca.Contacted).To(Equal(true))
+		})
+	})
+})
--- operation/operation_test.go
+++ operation/operation_test.go
@@ -1,1 +1,34 @@
 package operation
+
+import (
+	cs "CRM_backend/customer"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Unit Test for Operation")
+}
+
+var _ = Describe("Operation", func() {
+
+	BeforeEach(func() {
+
+	})
+	AfterEach(func() {
+
+	})
+
+	Context("customer functions", func() {
+		It("Test GetCustomers", func() {
+			ca := cs.CreateCustomer("1", "Andy", "Developer", "S", "S", true)
+			cb := cs.CreateCustomer("2", "Peter", "Developer", "S", "S", true)
+			Customers[ca.ID] = *ca
+			Customers[cb.ID] = *cb
+
+		})
+
+	})
+})
